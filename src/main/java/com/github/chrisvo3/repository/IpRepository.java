8d5b34cb239a552637783ffd9bca0c4c7949089b
Chris Vo <vochris3@gmail.com>
2020-05-09 23:05:38 -0400

    0509-2020.updae spring boot dependencies

--- src/main/java/com/github/chrisvo3/repository/IpRepository.java
+++ src/main/java/com/github/chrisvo3/repository/IpRepository.java
@@ -1,5 +1,7 @@
 package com.github.chrisvo3.repository;
 
+import com.github.chrisvo3.model.IpAdress;
+
 /**
  * Created by vochr
  * 09 May 2020 at 22 : 56
@@ -8,5 +10,5 @@ package com.github.chrisvo3.repository;
  * @project geolocation
  */
 
-public interface IpRepository {
+public interface IpRepository extends CrudRepository<IpAdress, Long> {
 }
